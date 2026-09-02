e00146243108832cbdae99ee95c200a0f6eea92a
Wilson <81046123+a36973@users.noreply.github.com>
2026-09-02 10:49:29 +0000

    Update_Ciclos

---
+++ Python/Ciclos/If.py
@@ -0,0 +1,11 @@
+x = int(input("Insira um número inteiro: "))
+
+if x < 0:
+    
+    print('Negativo ')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Um')
+else:
+    print('possitivo superior a 1')
