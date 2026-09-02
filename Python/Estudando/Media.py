2f014ccdda705bdd2882e551c332c1bcfab9333d
Wilson <81046123+a36973@users.noreply.github.com>
2026-09-02 14:38:47 +0000

    Updates

---
+++ Python/Estudando/Media.py
@@ -0,0 +1,9 @@
+def calcular_media(numeros):
+    return sum(numeros) / len(numeros)
+
+
+valores= [10, 20, 30]
+
+resultado = calcular_media(valores)
+
+print("A média de valores é:", resultado)
---
+++ Python/Estudando/Media_for.py
@@ -0,0 +1,11 @@
+frascos = [12, 15, 8, 20, 17]
+
+total = 0
+
+for quantidade in frascos:
+    total = total + quantidade
+
+print(total)
+
+
+
