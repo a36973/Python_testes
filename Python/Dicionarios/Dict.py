f8f5a6172e671df37ac71ab3cd7903e3180fc97e
Wilson <81046123+a36973@users.noreply.github.com>
2026-09-03 10:39:39 +0000

    Dicionario

---
+++ Python/Dicionarios/Dict.py
@@ -0,0 +1,9 @@
+frasco = {
+    "id": "F001",
+    "plantas": 12,
+    "estado": "adequado"
+}
+
+print("ID:", frasco["id"])
+print("Plantas:", frasco["plantas"])
+print("Estado:", frasco["estado"])
